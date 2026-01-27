2e35674e7d5b649cf44fbdf507f0b1ecebdae040
jadhavpratik5433 <jadhavpratik5433@gmail.com>
2026-01-27 15:35:56 +0530

    k

---
+++ eCommerce.sharedlibrary/ProductApi.Infrastructure/Repositorice/ProductRepository.cs
@@ -0,0 +1,43 @@
+﻿using Ecommerces.sherd.Responses;
+using ProductApi.Application.Interface;
+using ProductApi.Domain.Entity;
+using System;
+using System.Collections.Generic;
+using System.Linq.Expressions;
+using System.Text;
+
+namespace ProductApi.Infrastructure.Repositorice
+{
+    internal class ProductRepository : IProduct
+    {
+        public Task<Response> CreateAsync(Product entity)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<Response> DeleteAsync(int id)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<IEnumerable<Product>> GetAllAsync()
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<Product> GetByAsync(Expression<Func<Product, bool>> expression)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<Product> GetByIdAsync(int id)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<Response> UpdateAsync(Product entity)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
